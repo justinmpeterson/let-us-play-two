bf23cb4bc0e5b1d6496aaf80a55659d2b5ae2db3
Justin Peterson <justinmpeterson@gmail.com>
2017-11-28 22:17:06 -0600

    Getting started

---
+++ player.py
@@ -0,0 +1,25 @@
+class Player:
+    def __init__(self, p_Id, p_First, p_Last, p_Pos, p_Class):
+        self.info = {"player_id": p_Id,
+            "first_name": p_First,
+            "last_name": p_Last,
+            "position": p_Pos,
+            "classification": p_Class}
+        self.bat_stats = {"pa": 0,
+            "ab": 0,
+            "h": 0,
+            "2b": 0,
+            "3b": 0,
+            "hr": 0,
+            "so": 0,
+            "bb": 0,
+            "r": 0}
+        self.field_stats = {"ch": 0,
+            "po": 0,
+            "ast": 0,
+            "e": 0}
+        self.pit_stats = {"bfp": 0,
+            "outs": 0,
+            "so": 0,
+            "bb": 0,
+            "r": 0}
---
+++ team.py
@@ -0,0 +1,18 @@
+from player import Player
+
+class Team:
+    def __init__(self, p_Id, p_Location, p_Name, p_Abbr, p_Home, p_Players):
+        players = {}
+
+        self.info = {"team_id": p_Id,
+            "team_location": p_Location,
+            "team_name": p_Name,
+            "team_abbr": p_Abbr,
+            "is_home": p_Home}
+
+        self.game_info = {"r": 0,
+            "h": 0,
+            "e": 0}
+
+        for k,v in p_Players.items():
+            players[k] = v

dca0559c6ac3fd9cc552822d9d62bad2347e7e26
Justin Peterson <justinmpeterson@gmail.com>
2017-11-28 23:14:21 -0600

    Initial dev

--- game.py
+++ game.py
@@ -2,8 +2,33 @@ from player import Player
 from team import Team
 
 class Game:
+    def reset_hinning_stats(self):
+        self.hinning_stats["half_inning"] += 1
+        self.hinning_stats["tb"] = "Top" if self.hinning_stats["half_inning"] % 2 == 1 else "Bottom"
+        self.hinning_stats["ntb"] = "Top" if self.hinning_stats["tb"] == "Bottom" else "Bottom"
+        self.hinning_stats["r"] = 0
+        self.hinning_stats["h"] = 0
+        self.hinning_stats["e"] = 0
+        self.hinning_stats["run_1st"] = 0
+        self.hinning_stats["run_2nd"] = 0
+        self.hinning_stats["run_3rd"] = 0
+        self.hinning_stats["base_situation"] = 0
+
     def __init__(self, p_Id):
-        self.game_info = {"game_id": p_Id}
+        self.game_info = {"game_id": p_Id,
+            "bat_slots": {"Top": 0, "Bottom": 0},
+            "pit_slots": {"Top": 0, "Bottom": 0}}
+
+        self.hinning_stats = {"half_inning": 1,
+            "tb": "Top",
+            "ntb": "Bottom",
+            "r": 0,
+            "h": 0,
+            "e": 0,
+            "run_1st": 0,
+            "run_2nd": 0,
+            "run_3rd": 0,
+            "base_situation": 0}
 
         a_players = {}
         a_players[1] = Player(1, "Willie", "Mays", "CF", "STAR")
---
+++ play_game.py
@@ -0,0 +1,7 @@
+from player import Player
+from team import Team
+from game import Game
+
+if __name__ == "__main__":
+    game_id = 1
+    lpt_game = Game(1)
